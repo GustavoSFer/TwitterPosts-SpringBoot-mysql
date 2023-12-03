4d71c03e810cc756bec133fc7ed03b9a1029c299
GustavoSFer <gustavo.sfernander@gmail.com>
2023-12-02 22:26:50 -0300

    Feat: Iniciando o projeto

---
+++ src/main/java/com/fernandes/twitterPostMysql/TwitterPostsSpringBootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.fernandes.twitterPostMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPostsSpringBootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterPostsSpringBootMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandes/twitterPostMysql/TwitterPostsSpringBootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandes.twitterPostMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterPostsSpringBootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
