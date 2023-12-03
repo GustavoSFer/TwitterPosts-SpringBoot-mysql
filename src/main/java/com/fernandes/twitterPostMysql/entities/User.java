e8c7a68390811104892753484d6854a783beefa1
GustavoSFer <gustavo.sfernander@gmail.com>
2023-12-02 22:51:58 -0300

    Feat: Criando as anotações

--- src/main/java/com/fernandes/twitterPostMysql/entities/User.java
+++ src/main/java/com/fernandes/twitterPostMysql/entities/User.java
@@ -3,8 +3,18 @@ package com.fernandes.twitterPostMysql.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
