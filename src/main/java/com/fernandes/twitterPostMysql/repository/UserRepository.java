de13351eb5b877c30e8a23cfad56b446aae2e41a
GustavoSFer <gustavo.sfernander@gmail.com>
2023-12-02 22:52:59 -0300

    Feat: Criando o Repository de user

--- src/main/java/com/fernandes/twitterPostMysql/repository/UserRepository.java
+++ src/main/java/com/fernandes/twitterPostMysql/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.fernandes.twitterPostMysql.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fernandes.twitterPostMysql.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
