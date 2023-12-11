231c6efe97064b6aeb9f11d77d66c5cc1ec64691
GustavoSFer <gustavo.sfernander@gmail.com>
2023-12-11 20:44:59 -0300

    Feat: Criando o try no update

--- src/main/java/com/fernandes/twitterPostMysql/service/UserService.java
+++ src/main/java/com/fernandes/twitterPostMysql/service/UserService.java
@@ -11,23 +11,25 @@ import com.fernandes.twitterPostMysql.repository.UserRepository;
 import com.fernandes.twitterPostMysql.service.exception.DatabaseException;
 import com.fernandes.twitterPostMysql.service.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		List<User> users = userRepository.findAll();
 		return users;
 	}
-	
+
 	public User findById(Integer id) {
 		Optional<User> user = userRepository.findById(id);
-		
+
 		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User create(User obj) {
 		String nome = obj.getName();
 		if (obj.getName().isEmpty() || obj.getEmail().isEmpty()) {
@@ -35,18 +37,22 @@ public class UserService {
 		}
 		return userRepository.save(obj);
 	}
-	
+
 	public User update(Integer id, User obj) {
-		User user = findById(id);
-		updateUser(user, obj);
-		return userRepository.save(user);
+		try {
+			User user = findById(id);
+			updateUser(user, obj);
+			return userRepository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public void updateUser(User user, User obj) {
 		user.setName(obj.getName());
-		user.setEmail(obj.getEmail());		
+		user.setEmail(obj.getEmail());
 	}
-	
+
 	public User delete(Integer id) {
 		User user = findById(id);
 		userRepository.delete(user);
