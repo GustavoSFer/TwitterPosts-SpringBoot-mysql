5ad8eb0aa6bdf9815f62231017b90a840d0f1ac0
GustavoSFer <gustavo.sfernander@gmail.com>
2023-12-16 17:55:02 -0300

    Feat: Criando o findById de post

--- src/main/java/com/fernandes/twitterPostMysql/service/PostService.java
+++ src/main/java/com/fernandes/twitterPostMysql/service/PostService.java
@@ -1,12 +1,15 @@
 package com.fernandes.twitterPostMysql.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.twitterPostMysql.entities.Post;
+import com.fernandes.twitterPostMysql.entities.User;
 import com.fernandes.twitterPostMysql.repository.PostRepository;
+import com.fernandes.twitterPostMysql.service.exception.ResourceNotFoundException;
 
 @Service
 public class PostService {
@@ -17,6 +20,12 @@ public class PostService {
 	public List<Post> findAll() {
 		List<Post> posts = postRepository.findAll();
 		return posts;
+	}
+	
+	public Post findById(Integer id) {
+		Optional<Post> post = postRepository.findById(id);
+		
+		return post.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 }
